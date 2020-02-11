c066187d2ac0e54004484867bf1f73668eaaf9f0
Student <emilywright212@gmail.com>
2020-02-11 15:00:26 +0000

    second tests added

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -2,7 +2,7 @@ feature 'viewing bookmarks' do
   scenario 'it shows me all the bookmarks' do 
     visit('/bookmarks')
     expect(page).to have_content "http://www.makersacademy.com"
-    # expect(page).to have_content "bookmark 2"
+    expect(page).to have_content "http://www.google.com"
     # expect(page).to have_content "bookmark 3"
   end 
 end
